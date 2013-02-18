174ff4ffc8a6346867113de1ea51e1850285aa2c
Jared <jarcoal@gmail.com>
2013-02-17 22:04:04 -0800

    setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="django-globetrotter",
+    version="0.0.1",
+    description="Extra i18n + l10n tools for Django",
+    long_description="Extra i18n + l10n tools for Django",
+    keywords="django, i18n, l10n",
+    author="Jared Morse <jarcoal@gmail.com>",
+    author_email="jarcoal@gmail.com",
+    url="https://github.com/jarcoal/django-globetrotter",
+    license="BSD",
+    packages=["globetrotter"],
+    zip_safe=False,
+    install_requires=[],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+)
