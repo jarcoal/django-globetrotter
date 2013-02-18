ec7fa160d340136a560389f44cfeabd258471e57
Jared <jarcoal@gmail.com>
2013-02-18 01:14:57 -0800

    SubdomainLanguageMiddleware

--- globetrotter/middleware.py
+++ globetrotter/middleware.py
@@ -1,9 +1,14 @@
 from django.conf import settings
 from django.utils import translation, timezone
 from django.utils.translation import ugettext as _
+from django.http import Http404
 
-get_user_language = settings.GLOBETROTTER_GET_USER_LANGUAGE or lambda user: user.language
-get_user_time_zone = settings.GLOBETROTTER_GET_USER_TIME_ZONE or lambda user: user.time_zone
+get_user_language = getattr(settings, 'GLOBETROTTER_GET_USER_LANGUAGE', lambda user: user.language)
+get_user_time_zone = getattr(settings, 'GLOBETROTTER_GET_USER_TIME_ZONE', lambda user: user.time_zone)
+
+SUBDOMAIN_LANGUAGE_DEFAULTS = getattr(settings, 'GLOBETROTTER_SUBDOMAIN_LANGUAGE_DEFAULTS', ['www'])
+SUBDOMAIN_LANGUAGE_RAISE_404 = getattr(settings, 'GLOBETROTTER_SUBDOMAIN_LANGUAGE_RAISE_404', True)
+
 
 class UserLanguageMiddleware(object):
     """
@@ -31,3 +36,20 @@ class UserTimeZoneMiddleware(object):
 
         if request.user.is_authenticated():
             timezone.activate(get_user_time_zone(request.user))
+
+class SubdomainLanguageMiddleware(object):
+    """
+    Activates language based on subdomain.
+    Ex: http://en.example.com/
+    """
+
+    def process_request(self, request):
+        domain = request.get_host().split(':')[0]
+        subdomain = '.'.join(domain.split('.')[:-2])
+
+        translation.activate(settings.LANGUAGE_CODE)
+
+        if subdomain and subdomain in [language_code for language_code in settings.LANGUAGES]:
+            translation.activate(subdomain)
+        elif SUBDOMAIN_LANGUAGE_RAISE_404 and subdomain not in SUBDOMAIN_LANGUAGE_DEFAULTS:
+            raise Http404
