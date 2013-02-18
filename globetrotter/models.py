db6bd39f4919a104dde465c681eb608b8d984442
Jared <jarcoal@gmail.com>
2013-02-18 00:42:11 -0800

    TimeZoneMixin, LanguageMixin view mixins

--- globetrotter/models.py
+++ globetrotter/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import fields
 
-# Create your models here.
+
+class TimeZoneMixin(models.Model):
+    """
+    Add time zone support to a model.
+    """
+    time_zone = fields.TimeZoneField()
+
+    class Meta:
+        abstract = True
+
+class LanguageMixin(models.Model):
+	"""
+	Add language support to a model.
+	"""
+	language = fields.LanguageField()
+
+	class Meta:
+		abstract = True
