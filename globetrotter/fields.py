c76ba21fd600f4be42c9df4abe4a698f05dbe385
Jared <jarcoal@gmail.com>
2013-03-12 18:44:12 -0700

    fix max_length issue, add default verbose_name, south introspection rules

--- globetrotter/fields.py
+++ globetrotter/fields.py
@@ -12,17 +12,20 @@ class TimeZoneField(models.CharField):
     Stores pytz.timezone objects in the database.
     """
 
-    max_length = 64
     description = _('Stores pytz.timezone objects in the database.')
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, verbose_name=None, **kwargs):
         """
         choices: COMMON_TIME_ZONE_CHOICES
         default: settings.TIME_ZONE
         """
+
+        verbose_name = verbose_name or _('time zone')
+        kwargs['max_length'] = kwargs.get('max_length', 64)
         kwargs['choices'] = kwargs.get('choices', COMMON_TIME_ZONE_CHOICES)
         kwargs['default'] = kwargs.get('default', settings.TIME_ZONE)
-        return super(TimeZoneField, self).__init__(*args, **kwargs)
+
+        return super(TimeZoneField, self).__init__(verbose_name, **kwargs)
 
     def to_python(self, value):
         """
@@ -42,14 +45,24 @@ class LanguageField(models.CharField):
     Stores language codes in two char ("en") or 5 char ("en_US", "en-us") format.
     """
 
-    max_length = 5
     description = _('Stores language codes in two char ("en") or 5 char ("en_US", "en-us") format.')
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, verbose_name=None, **kwargs):
         """
         choices: settings.LANGUAGE
         default: settings.LANGUAGE_CODE
         """
+
+        verbose_name = verbose_name or _('language')
+        kwargs['max_length'] = kwargs.get('max_length', 5)
         kwargs['choices'] = kwargs.get('choices', settings.LANGUAGES)
         kwargs['default'] = kwargs.get('default', settings.LANGUAGE_CODE)
-        return super(LanguageField, self).__init__(*args, **kwargs)
+
+        return super(LanguageField, self).__init__(verbose_name, **kwargs)
+
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^globetrotter\.fields\.(TimeZoneField|LanguageField)"])
+except ImportError:
+    pass
