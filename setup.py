a86cef0e42f4db684648c8a88bebb2d60d12075d
Jared <jarcoal@gmail.com>
2013-07-20 15:34:20 -0700

    bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="django-globetrotter",
-    version="0.0.4",
+    version="0.0.5",
     description="Extra i18n + l10n tools for Django",
     long_description="Extra i18n + l10n tools for Django",
     keywords="django, i18n, l10n",
